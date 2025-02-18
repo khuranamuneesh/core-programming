bc2b6cdec0d29d1331194b815c4cef0900056937
khuranamuneesh <code.muneeshdev.career@gmail.com>
2025-02-18 09:35:25 +0530

    level-1 java array

---
+++ java-arrays/level-1/VotingEligibility.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class VotingEligibility {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] ages = new int[10];
+
+        // Taking input for 10 students
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            ages[i] = scanner.nextInt();
+        }
+
+        // Checking voting eligibility
+        for (int age : ages) {
+            if (age < 0) {
+                System.out.println("Invalid age: " + age);
+            } else if (age >= 18) {
+                System.out.println("The student with the age " + age + " can vote.");
+            } else {
+                System.out.println("The student with the age " + age + " cannot vote.");
+            }
+        }
+
+        scanner.close();
+    }
+}
